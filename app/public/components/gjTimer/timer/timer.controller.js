a32a2de05b16b42ec82260454a8fe1e51a96ee6e
timwongj <wong.t@husky.neu.edu>
2015-10-26 21:07:45 -0400

    fixed timer logic bug

--- app/public/components/gjTimer/timer/timer.controller.js
+++ app/public/components/gjTimer/timer/timer.controller.js
@@ -4,62 +4,50 @@
 
   function TimerController($scope, $rootScope, $interval, $timeout, TimerService) {
 
-    var self = this;
+    var self = this, timer, state = 'reset';
 
-    var TIMER_REFRESH_INTERVAL = 50;
-    var SPACE_BAR_KEY_CODE = 32;
-    var COLOR_BLACK = '#000000';
-    var COLOR_RED = '#FF0000';
-    var COLOR_GREEN = '#2EB82E';
-    var DEFAULT_TIMER_DISPLAY = moment(0).format('s.SS');
-    var FADE_ANIMATION_DELAY = 200;
-    var timer, keydown = false, isTiming = false, falseStart = false;
+    var TIMER_REFRESH_INTERVAL = 50, FADE_ANIMATION_DELAY = 200, SPACE_BAR_KEY_CODE = 32;
 
-    self.time = DEFAULT_TIMER_DISPLAY;
-    self.timerStyle = { 'color': '#000000' };
+    self.time = moment(0).format('s.SS');
 
-    $scope.$on('keydown', function(event, args) {
-      if (!keydown) {
-        if (!isTiming && (args.keyCode === 32)) {
-          self.time = DEFAULT_TIMER_DISPLAY;
-          self.timerStyle = { 'color': COLOR_RED };
-          $timeout(function() {
-            if (!falseStart) {
-              keydown = true;
-              self.timerStyle = { 'color': COLOR_GREEN };
-              $rootScope.$broadcast('timer focus');
-            } else {
-              falseStart = false;
-              self.timerStyle = { 'color': COLOR_BLACK };
-            }
-          }, FADE_ANIMATION_DELAY);
-        } else if (isTiming) {
-          keydown = true;
-          $interval.cancel(timer);
-          $rootScope.$broadcast('timer unfocus');
-          TimerService.saveResult(self.time, $rootScope.scramble, $rootScope.sessionId);
-          $rootScope.$broadcast('new scramble', $rootScope.puzzle);
-          $rootScope.$broadcast('refresh data');
-        }
+    var STYLES = {
+      BLACK: { 'color': '#000000' },
+      RED: { 'color': '#FF0000' },
+      GREEN: { 'color': '#2EB82E' }
+    };
+
+    $scope.$on('keydown', function($event, event) {
+      if ((state === 'reset') && (event.keyCode === SPACE_BAR_KEY_CODE)) {
+        state = 'keydown';
+        self.time = moment(0).format('s.SS');
+        self.timerStyle = STYLES.RED;
+        $timeout(function() {
+          if (state === 'keydown') {
+            state = 'ready';
+            self.timerStyle = STYLES.GREEN;
+            $rootScope.$broadcast('timer focus');
+          }
+        }, FADE_ANIMATION_DELAY);
+      } else if (state === 'timing') {
+        state = 'stopped';
+        $interval.cancel(timer);
+        $rootScope.$broadcast('timer unfocus');
+        TimerService.saveResult(self.time, $rootScope.scramble, $rootScope.sessionId);
+        $rootScope.$broadcast('refresh data');
+        $rootScope.$broadcast('new scramble', $rootScope.puzzle);
       }
     });
 
     $scope.$on('keyup', function($event, event) {
-      if (keydown) {
-        if (!isTiming && (event.keyCode === SPACE_BAR_KEY_CODE)) {
-          keydown = false;
-          isTiming = true;
-          self.timerStyle = { 'color': COLOR_BLACK };
-          TimerService.startTimer();
-          timer = $interval(function() {
-            self.time = TimerService.getTime();
-          }, TIMER_REFRESH_INTERVAL);
-        } else if (isTiming) {
-          isTiming = false;
-          keydown = false;
-        }
-      } else {
-        falseStart = (self.timerStyle.color !== COLOR_GREEN);
+      self.timerStyle = STYLES.BLACK;
+      if ((state === 'ready') && (event.keyCode === SPACE_BAR_KEY_CODE)) {
+        state = 'timing';
+        TimerService.startTimer();
+        timer = $interval(function() {
+          self.time = TimerService.getTime();
+        }, TIMER_REFRESH_INTERVAL);
+      } else if ((state === 'stopped') || (state === 'keydown')) {
+        state = 'reset';
       }
     });
 
