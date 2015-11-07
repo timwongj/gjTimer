cfc33dbb868c142577fc6ca8ca0cdc5bd7e331b3
timwongj <wong.t@husky.neu.edu>
2015-11-06 20:19:04 -0500

    cleaned up settings directive

--- app/public/components/gjTimer/menuBar/menuBar.service.js
+++ app/public/components/gjTimer/menuBar/menuBar.service.js
@@ -10,11 +10,11 @@
 
     var DEFAULT_SETTINGS = {
       input: 'Timer',
-      saveScrambles: true,
+      saveScrambles: 'Yes',
       timerStartDelay: 0,
       timerStopDelay: 100,
       resultsPrecision: 2,
-      statisticsPrecision: 2,
+      statisticsPrecision: 3,
       timerPrecision: 3,
       timerRefreshInterval: 50
     };
